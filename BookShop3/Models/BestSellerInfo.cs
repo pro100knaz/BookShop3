7ac14dc0baeb6c79e4badd601e1bff59955ebf31
MFlEGENDANTON <jffgx@mail.ru>
2024-05-07 21:09:18 +0300

    Добавлена коллекция лучший книг по продажам в ObservableCollection

---
+++ BookShop3/Models/BestSellerInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookShop3.Dal.Entities;
+
+namespace BookShop3.Models
+{
+    internal class BestSellerInfo
+    {
+        public Book Book { get; set; }
+
+        public int SellCount { get; set; }
+    }
+}
--- BookShop3/ViewModels/StatisticViewModel.cs
+++ BookShop3/ViewModels/StatisticViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using BookShop.Interfaces;
 using BookShop3.Dal.Entities;
+using BookShop3.Models;
 using MathCore.WPF.Commands;
 using MathCore.WPF.ViewModels;
 using Microsoft.EntityFrameworkCore;
@@ -15,46 +17,55 @@ namespace BookShop3.ViewModels
     internal class StatisticViewModel : ViewModel
     {
 
-            #region int BooksCount - "Количество книг"
 
-        ///<summary> Количество книг </summary>
-        private int _BooksCount;
-
-        ///<summary> Количество книг </summary>
-        public int BooksCount
-        {
-            get => _BooksCount;
-            set => Set(ref _BooksCount, value);
-        }
-
-        #endregion
+        public ObservableCollection<BestSellerInfo> BestSellers { get;} = new ObservableCollection<BestSellerInfo>();
 
 
         #region Command ComputeStatisticCommand - Вычисление статистических данных
 
         ///<summary> Вычисление статистических данных </summary>
         private ICommand _ComputeStatisticCommand;
-
         ///<summary> Вычисление статистических данных </summary>
         public ICommand ComputeStatisticCommand => _ComputeStatisticCommand ??=
             new LambdaCommand(OnComputeStatisticCommandExecuted, CanComputeStatisticCommandExecute);
-
         ///<summary>Проверка возможности выполнения - Вычисление статистических данных </summary>
         private bool CanComputeStatisticCommandExecute(object p) => true;
-
         ///<summary>Логика выполнения - Вычисление статистических данных </summary>
         private async void OnComputeStatisticCommandExecuted(object p)
         {
-            BooksCount = await _booksRepository.Items.CountAsync();
 
-            var deals =  _dealsRepository.Items;
+            await ComputeDealsStatistic();
 
-            var bestsellers = await deals.GroupBy(deal => deal.Book)
-            .Select(book_deals => new { Book = book_deals.Key, Count = book_deals.Count() })
-            .OrderByDescending(book => book.Count)
-            .Take(5)
-            .ToArrayAsync();
         }
+
+        private async Task ComputeDealsStatistic()
+        {
+            var _deals = _dealsRepository.Items;
+
+            var bestsellers = await _deals
+                .GroupBy(deal => deal.Book)
+                .Select(book_deals => new { Book = book_deals.Key, Count = book_deals.Count() })
+                .OrderByDescending(book => book.Count)
+                .Take(5)
+                .ToArrayAsync();
+
+            var bestsellers2 =  _deals
+                .GroupBy(deal => deal.Book.Id)
+                .Select(deals => new { BookId = deals.Key, Count = deals.Count() })
+                .OrderByDescending(deals => deals.Count)
+                .Take(5)
+                .Join(_booksRepository.Items,
+                    deals => deals.BookId,
+                    book => book.Id,
+                    (deals, book) => new BestSellerInfo() { Book = book, SellCount = deals.Count });
+
+            BestSellers.Clear();
+            foreach (var bestSeller in await bestsellers2.ToArrayAsync())
+            {
+                BestSellers.Add(bestSeller);
+            }
+        }
+
 
         #endregion
 
